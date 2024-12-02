1eccc46896af520ca0c17e171896accd2d280d6e
Msanouvo <43829596+MSanouvo@users.noreply.github.com>
2024-12-02 14:48:34 -0800

    added images/icons for weather conditons
    - assests will help convey weather condition more
    than words will
    - Background/Icons change based on the day's given
    weather conditon listed by the API (under icon)
    - added function to change between fahernheit and
    celsius
    - added info for future days in the week

--- src/weather.js
+++ src/weather.js
@@ -1,10 +1,16 @@
-import { createDataElements } from "."
+import { createDataElements, changeBackground } from '.';
 
-async function getWeather(location){
-    const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+location+'?unitGroup=us&key=DBEL78GQRDBKP5JNA4PP2STRF&contentType=json&elements=tempmax,tempmin,temp,description')
-    const weatherData = await response.json()
-    console.log(weatherData)
-    createDataElements(weatherData)
+async function getWeather(location) {
+  const response = await fetch(
+    'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' +
+      location +
+      '?unitGroup=us&key=DBEL78GQRDBKP5JNA4PP2STRF&contentType=json'
+  );
+  const weatherData = await response.json();
+  console.log(weatherData);
+  changeBackground(weatherData)
+  createDataElements(weatherData);
+  
 }
 
-export{getWeather}
+export { getWeather };
